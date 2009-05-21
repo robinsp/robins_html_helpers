5d4d5406c2eb14d6775cce1719690c3b5e2f5459
Robin Spainhour <robin@robinspainhour.com>
2009-05-21 15:15:10 +0200

    Removed wierd 'super' mock from specs

--- lib/robins_html_helpers/form_builder.rb
+++ lib/robins_html_helpers/form_builder.rb
@@ -3,42 +3,37 @@ module RobinsHtmlHelpers
     
     def text_field(method, options = {}) 
       label_text = options.delete(:label)
-      wrap_field(method, get_super.text_field(method, options), label_text )
+      wrap_field(method, super(method, options), label_text )
     end
     
     def text_area(method, options = {}) 
       label_text = options.delete(:label)
-      wrap_field(method, get_super.text_area(method, options), label_text )
+      wrap_field(method, super(method, options), label_text )
     end
     
     def password_field(method, options = {}) 
       label_text = options.delete(:label)
-      wrap_field(method, get_super.password_field(method, options), label_text )
+      wrap_field(method, super(method, options), label_text )
     end
     
     def date_select(method, options = {})
       label_text = options.delete(:label)
-      wrap_field(method, get_super.date_select(method, options), label_text )
+      wrap_field(method, super(method, options), label_text )
     end
     
     def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
       label_text = options.delete(:label)
-      wrap_field(method, get_super.check_box(method, options, checked_value, unchecked_value), label_text )
+      wrap_field(method, super(method, options, checked_value, unchecked_value), label_text )
     end
     
     def select(method, choices, options = {}, html_options = {})
       label_text = html_options.delete(:label)
-      wrap_field(method, get_super.select(method, choices, options, html_options), label_text )
+      wrap_field(method, super(method, choices, options, html_options), label_text )
     end
   
     private 
-    # Not sure how to mock 'super', so this is the work-around used by the specs
-    def get_super
-      super
-    end
-    
     def wrap_field(method, form_field, label_text)
-      "<div class=\"custom_form_field\">#{get_super.label(method, label_text )} #{ form_field }</div>"
+      "<div class=\"custom_form_field\">#{label(method, label_text )} #{ form_field }</div>"
     end
   end
   
--- spec/models/form_builder_spec.rb
+++ spec/models/form_builder_spec.rb
@@ -5,27 +5,37 @@ describe RobinsHtmlHelpers::FormBuilder do
   before do 
     @expected_attrib = :the_attrib
     
-    @builder = RobinsHtmlHelpers::FormBuilder.new(nil, nil, nil, nil, nil)
-    @mock_super = mock
-    @mock_super.stubs(:label).returns("label-result")
     
-    @builder.stubs(:get_super).returns(@mock_super)
+    @mock_template = mock("the-template")
+    @mock_object_name = mock('object-name')
+    @mock_the_object = mock('the-object')
+    
+    @builder = RobinsHtmlHelpers::FormBuilder.new(@mock_object_name, 
+                                                  @mock_the_object, 
+                                                  @mock_template, 
+                                                  nil, nil)
+                                                  
+    @mock_template.stubs(:label).returns("label-result")
+    @builder.stubs(:get_super).returns(@mock_template)
     
   end
   
   it "should remove label param before delegating to super" do 
-    expected_opts = {:opt => "val" }
-    opts_with_label = expected_opts.merge(:label => "thelabel")
+    expected_opts = {:object => @mock_the_object, :opt => "val" }
+    opts_with_label = {:opt => "val", :label => "thelabel"}
     
-    [:text_field, :text_area, :password_field, :date_select].each do |method| 
-      @mock_super.expects(method).with(@expected_attrib, expected_opts)
+    [:text_field, :text_area, :password_field].each do |method| 
+      @mock_template.expects(method).with(@mock_object_name, @expected_attrib, expected_opts)
       @builder.send method, @expected_attrib, opts_with_label
     end
     
-    @mock_super.expects(:check_box).with(@expected_attrib, expected_opts, 1.to_s, 0.to_s)
+    @mock_template.expects(:date_select).with(@mock_object_name, @expected_attrib, expected_opts, {})
+    @builder.send :date_select, @expected_attrib, opts_with_label
+    
+    @mock_template.expects(:check_box).with(@mock_object_name, @expected_attrib, expected_opts, 1.to_s, 0.to_s)
     @builder.send :check_box, @expected_attrib, opts_with_label
     
-    @mock_super.expects(:select).with(@expected_attrib, {}, {}, expected_opts)
+    @mock_template.expects(:select).with(@mock_object_name, @expected_attrib, {}, {:object => @mock_the_object}, {:opt => "val" })
     @builder.send :select, @expected_attrib, {}, {}, opts_with_label
   end
   
