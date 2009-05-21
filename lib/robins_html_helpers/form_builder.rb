db29c3846abef5d25e2b49d086b0c08a840f1694
Robin Spainhour <robin@robinspainhour.com>
2009-05-21 17:25:38 +0200

    Satisfied some pending specs

--- lib/robins_html_helpers/form_builder.rb
+++ lib/robins_html_helpers/form_builder.rb
@@ -3,12 +3,29 @@ module RobinsHtmlHelpers
     
     def text_field(method, options = {}) 
       label_text = options.delete(:label)
-      wrap_field(method, super(method, options), label_text )
+      hint, hint_class = options.delete(:hint), options.delete(:hint_class)
+      the_dom_id = options[:id] || object_name + "_" + method.to_s
+      raise "No :hint supplied" if hint_class && hint.blank?
+
+      result = wrap_field(method, super(method, options), label_text )
+      result << hint_js_tag(the_dom_id, hint, hint_class) unless hint.blank?
+      result
     end
     
     def text_area(method, options = {}) 
       label_text = options.delete(:label)
-      wrap_field(method, super(method, options), label_text )
+      hint, hint_class = options.delete(:hint), options.delete(:hint_class)
+      the_dom_id = options[:id] || object_name + "_" + method.to_s
+      
+      raise "No :hint supplied" if hint_class && hint.blank?
+
+      result = wrap_field(method, super(method, options), label_text )
+      result << hint_js_tag(the_dom_id, hint, hint_class) unless hint.blank?
+      result
+    end
+    
+    def hint_js_tag(a,b,c)
+      ""
     end
     
     def password_field(method, options = {}) 
--- spec/models/form_builder_spec.rb
+++ spec/models/form_builder_spec.rb
@@ -6,7 +6,7 @@ describe RobinsHtmlHelpers::FormBuilder do
     @expected_attrib = :the_attrib
     
     @mock_template = mock("the-template")
-    @mock_object_name = mock('object-name')
+    @mock_object_name = 'object-name'
     @mock_the_object = mock('the-object')
     
     @builder = RobinsHtmlHelpers::FormBuilder.new(@mock_object_name, 
@@ -38,13 +38,36 @@ describe RobinsHtmlHelpers::FormBuilder do
       
       
       describe "with hints" do 
-        it "should remove :hint option before delegating to super class"
+        it "should remove :hint and :hint_class options before delegating to super class" do 
+          @mock_template.expects(method).with(@mock_object_name, @expected_attrib, @expected_opts)
+          @builder.send method, @expected_attrib, {:opt => "val", :hint => "A hint", :hint_class => "CSS_class"}
+        end
         
-        it "should not allow :hint_class option without a :hint option"
+        it "should not allow :hint_class option without a :hint option" do 
+          lambda { @builder.send(method, @expected_attrib, {:hint_class => "CSS_class"}) }.should  raise_error "No :hint supplied"
+        end 
         
-        it "should append hint JS "
+        it "should append hint JS" do 
+          @mock_template.stubs(method)
+          @builder.stubs(:wrap_field).returns("field-result")
+          @builder.expects(:hint_js_tag).returns("hint-result")
+          @builder.send(method, @expected_attrib, {:hint => "A hint"}).should == "field-result" + "hint-result"
+        end
         
-        it "should create the hint JS by supplying an DOM element id"
+        it "should default :hint_class to 'hinting' " 
+        
+        it "should create the hint JS by supplying an DOM element id, hint and hint_class" do
+          @mock_template.stubs(method)
+          @builder.stubs(:wrap_field).returns("field-result")
+          
+          # When the DOM id is given by the developer
+          @builder.expects(:hint_js_tag).with("element_id", "A hint", "hint_class").returns("hint-result")
+          @builder.send method, @expected_attrib, {:id => "element_id", :hint => "A hint", :hint_class => "hint_class"}
+          
+          # When the DOM id is set by default
+          @builder.expects(:hint_js_tag).with("#{@mock_object_name}_#{@expected_attrib.to_s}", "A hint", "hint_class").returns("hint-result")
+          @builder.send method, @expected_attrib, {:hint => "A hint", :hint_class => "hint_class"}
+        end
         
       end 
     end
