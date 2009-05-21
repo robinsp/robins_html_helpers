d4281bf07c20b71ca82f0a237400e9ee4a327312
Robin Spainhour <robin@robinspainhour.com>
2009-05-21 17:31:39 +0200

    refactored

--- spec/models/form_builder_spec.rb
+++ spec/models/form_builder_spec.rb
@@ -38,6 +38,11 @@ describe RobinsHtmlHelpers::FormBuilder do
       
       
       describe "with hints" do 
+        before do 
+          @mock_template.stubs(method)
+          @builder.stubs(:wrap_field).returns("field-result")
+        end
+        
         it "should remove :hint and :hint_class options before delegating to super class" do 
           @mock_template.expects(method).with(@mock_object_name, @expected_attrib, @expected_opts)
           @builder.send method, @expected_attrib, {:opt => "val", :hint => "A hint", :hint_class => "CSS_class"}
@@ -48,25 +53,24 @@ describe RobinsHtmlHelpers::FormBuilder do
         end 
         
         it "should append hint JS" do 
-          @mock_template.stubs(method)
-          @builder.stubs(:wrap_field).returns("field-result")
           @builder.expects(:hint_js_tag).returns("hint-result")
           @builder.send(method, @expected_attrib, {:hint => "A hint"}).should == "field-result" + "hint-result"
         end
         
         it "should default :hint_class to 'hinting' " 
         
-        it "should create the hint JS by supplying an DOM element id, hint and hint_class" do
-          @mock_template.stubs(method)
-          @builder.stubs(:wrap_field).returns("field-result")
-          
-          # When the DOM id is given by the developer
-          @builder.expects(:hint_js_tag).with("element_id", "A hint", "hint_class").returns("hint-result")
-          @builder.send method, @expected_attrib, {:id => "element_id", :hint => "A hint", :hint_class => "hint_class"}
-          
-          # When the DOM id is set by default
-          @builder.expects(:hint_js_tag).with("#{@mock_object_name}_#{@expected_attrib.to_s}", "A hint", "hint_class").returns("hint-result")
-          @builder.send method, @expected_attrib, {:hint => "A hint", :hint_class => "hint_class"}
+        describe "with DOM id given by the developer" do 
+          it "should create the hint JS by supplying an DOM element id, hint and hint_class" do 
+            @builder.expects(:hint_js_tag).with("element_id", "A hint", "hint_class").returns("hint-result")
+            @builder.send method, @expected_attrib, {:id => "element_id", :hint => "A hint", :hint_class => "hint_class"}
+          end
+        end
+        
+        describe "When the DOM id is set by default" do 
+          it "should create the hint JS by supplying an DOM element id, hint and hint_class" do 
+            @builder.expects(:hint_js_tag).with("#{@mock_object_name}_#{@expected_attrib.to_s}", "A hint", "hint_class").returns("hint-result")
+            @builder.send method, @expected_attrib, {:hint => "A hint", :hint_class => "hint_class"}
+          end
         end
         
       end 
