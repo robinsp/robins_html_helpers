121630378a1737c06601b10926232eff5645e909
Robin Spainhour <robin@robinspainhour.com>
2009-05-21 15:29:08 +0200

    refactored

--- spec/models/form_builder_spec.rb
+++ spec/models/form_builder_spec.rb
@@ -5,7 +5,6 @@ describe RobinsHtmlHelpers::FormBuilder do
   before do 
     @expected_attrib = :the_attrib
     
-    
     @mock_template = mock("the-template")
     @mock_object_name = mock('object-name')
     @mock_the_object = mock('the-object')
@@ -18,33 +17,39 @@ describe RobinsHtmlHelpers::FormBuilder do
     @mock_template.stubs(:label).returns("label-result")
     @builder.stubs(:get_super).returns(@mock_template)
     
+    @expected_opts = {:object => @mock_the_object, :opt => "val" }
+    @opts_with_label = {:opt => "val", :label => "thelabel"}
+    
   end
   
   [:text_field, :text_area, :password_field].each do |method| 
     describe "#{method.to_s}()" do 
       it "should remove label param before delegating to super class" do 
-        expected_opts = {:object => @mock_the_object, :opt => "val" }
-        opts_with_label = {:opt => "val", :label => "thelabel"}
-        
-        @mock_template.expects(method).with(@mock_object_name, @expected_attrib, expected_opts)
-        @builder.send method, @expected_attrib, opts_with_label
+        @mock_template.expects(method).with(@mock_object_name, @expected_attrib, @expected_opts)
+        @builder.send method, @expected_attrib, @opts_with_label
       end
     end
   end
   
-  
-  it "should remove label param before delegating to super" do 
-    expected_opts = {:object => @mock_the_object, :opt => "val" }
-    opts_with_label = {:opt => "val", :label => "thelabel"}
-    
-    @mock_template.expects(:date_select).with(@mock_object_name, @expected_attrib, expected_opts, {})
-    @builder.send :date_select, @expected_attrib, opts_with_label
-    
-    @mock_template.expects(:check_box).with(@mock_object_name, @expected_attrib, expected_opts, 1.to_s, 0.to_s)
-    @builder.send :check_box, @expected_attrib, opts_with_label
-    
-    @mock_template.expects(:select).with(@mock_object_name, @expected_attrib, {}, {:object => @mock_the_object}, {:opt => "val" })
-    @builder.send :select, @expected_attrib, {}, {}, opts_with_label
+  describe "date_select()" do 
+    it "should remove label param before delegating to super class" do 
+      @mock_template.expects(:date_select).with(@mock_object_name, @expected_attrib, @expected_opts, {})
+      @builder.send :date_select, @expected_attrib, @opts_with_label  
+    end
   end
   
+  describe "select()" do 
+    it "should remove label param before delegating to super class" do 
+      @mock_template.expects(:select).with(@mock_object_name, @expected_attrib, {}, {:object => @mock_the_object}, {:opt => "val" })
+      @builder.send :select, @expected_attrib, {}, {}, @opts_with_label  
+    end
+  end
+  
+  describe "check_box()" do 
+    it "should remove label param before delegating to super class" do 
+      @mock_template.expects(:check_box).with(@mock_object_name, @expected_attrib, @expected_opts, 1.to_s, 0.to_s)
+      @builder.send :check_box, @expected_attrib, @opts_with_label  
+    end
+  end
+
 end
