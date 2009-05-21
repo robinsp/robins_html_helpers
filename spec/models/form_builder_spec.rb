8ae9082786d8ab3bbf5062f1f10e993d2e04fb02
Robin Spainhour <robin@robinspainhour.com>
2009-05-21 16:39:41 +0200

    Added pending specs

--- spec/models/form_builder_spec.rb
+++ spec/models/form_builder_spec.rb
@@ -18,42 +18,61 @@ describe RobinsHtmlHelpers::FormBuilder do
     @builder.stubs(:get_super).returns(@mock_template)
     
     @expected_opts = {:object => @mock_the_object, :opt => "val" }
-    @opts_with_label = {:opt => "val", :label => "thelabel"}
+    @opts_with_label = {:opt => "val", :label => "thelabel" }
     
   end
   
+  [:text_field, :text_area, :password_field, :date_select, :select, :check_box].each do |method|
+    describe "#{method.to_s}()" do 
+      it "should wrap_field"
+    end
+  end
+  
+  
   [:text_field, :text_area].each do |method| 
     describe "#{method.to_s}()" do 
-      it "should remove label param before delegating to super class" do 
+      it "should remove :label param before delegating to super class" do 
         @mock_template.expects(method).with(@mock_object_name, @expected_attrib, @expected_opts)
         @builder.send method, @expected_attrib, @opts_with_label
       end
+      
+      
+      describe "with hints" do 
+        it "should remove :hint option before delegating to super class"
+        
+        it "should not allow :hint_class option without a :hint option"
+        
+        it "should append hint JS "
+        
+        it "should create the hint JS by supplying an DOM element id"
+        
+      end 
     end
   end
   
   describe "password_field()" do 
-    it "should remove label param before delegating to super class" do 
+    it "should remove :label option before delegating to super class" do 
       @mock_template.expects(:password_field).with(@mock_object_name, @expected_attrib, @expected_opts)
       @builder.password_field(@expected_attrib, @opts_with_label)
     end
   end
   
   describe "date_select()" do 
-    it "should remove label param before delegating to super class" do 
+    it "should remove :label option before delegating to super class" do 
       @mock_template.expects(:date_select).with(@mock_object_name, @expected_attrib, @expected_opts, {})
       @builder.date_select(@expected_attrib, @opts_with_label)
     end
   end
   
   describe "select()" do 
-    it "should remove label param before delegating to super class" do 
+    it "should remove :label option before delegating to super class" do 
       @mock_template.expects(:select).with(@mock_object_name, @expected_attrib, {}, {:object => @mock_the_object}, {:opt => "val" })
       @builder.select( @expected_attrib, {}, {}, @opts_with_label )
     end
   end
   
   describe "check_box()" do 
-    it "should remove label param before delegating to super class" do 
+    it "should remove :label option before delegating to super class" do 
       @mock_template.expects(:check_box).with(@mock_object_name, @expected_attrib, @expected_opts, 1.to_s, 0.to_s)
       @builder.check_box( @expected_attrib, @opts_with_label )
     end
