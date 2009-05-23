feda74fed8d69e622cbbffc6366f0ee241b75de6
Robin Spainhour <robin@robinspainhour.com>
2009-05-23 12:33:00 +0200

    Refactored

--- lib/robins_html_helpers/form_builder.rb
+++ lib/robins_html_helpers/form_builder.rb
@@ -1,35 +1,50 @@
 module RobinsHtmlHelpers 
   class FormBuilder < ActionView::Helpers::FormBuilder
     
+    class HintOptions
+      attr_reader :hint, :hint_class
+      
+      def initialize(hint, hint_class)
+        raise "No :hint supplied" if hint_class && hint.blank?
+        @hint = hint
+        @hint_class = hint_class || "hinting"
+      end
+      
+      def enabled?
+        !hint.blank?
+      end
+      
+      def to_js(dom_element_id) 
+        "new RobinsHtmlHelpers.FormFieldHint('#{dom_element_id}', '#{hint}', '#{hint_class}');"  
+      end
+    end
+    
     def text_field(method, options = {}) 
       label_text = options.delete(:label)
-      hint, hint_class = options.delete(:hint), options.delete(:hint_class)
-      the_dom_id = options[:id] || object_name + "_" + method.to_s
-      raise "No :hint supplied" if hint_class && hint.blank?
+      hint_options = HintOptions.new(options.delete(:hint), options.delete(:hint_class))
 
       result = wrap_field(method, super(method, options), label_text )
-      result << hint_js_tag(the_dom_id, hint, (hint_class || "hinting")) unless hint.blank?
+      
+      if hint_options.enabled?
+        result << @template.javascript_tag( hint_options.to_js( get_dom_id_from(options, method) ) )
+      end
+      
       result
     end
     
     def text_area(method, options = {}) 
       label_text = options.delete(:label)
-      hint, hint_class = options.delete(:hint), options.delete(:hint_class)
-      the_dom_id = options[:id] || object_name + "_" + method.to_s
-      
-      raise "No :hint supplied" if hint_class && hint.blank?
+      hint_options = HintOptions.new(options.delete(:hint), options.delete(:hint_class))
 
       result = wrap_field(method, super(method, options), label_text )
-      result << hint_js_tag(the_dom_id, hint, (hint_class || "hinting")) unless hint.blank?
+
+      if hint_options.enabled?
+        result << @template.javascript_tag( hint_options.to_js( get_dom_id_from(options, method) ) )
+      end
+      
       result
     end
-    
-    def hint_js_tag(element_id, hint, hint_class)
-      raise "Missing arg" if element_id.blank? || hint.blank? || hint_class.blank?
-      script = "new RobinsHtmlHelpers.FormFieldHint('#{element_id}', '#{hint}', '#{hint_class}');"
-      @template.javascript_tag(script)
-    end
-    
+        
     def password_field(method, options = {}) 
       label_text = options.delete(:label)
       wrap_field(method, super(method, options), label_text )
@@ -53,6 +68,10 @@ module RobinsHtmlHelpers
     private
     def wrap_field(method, form_field, label_text)
       "<div class=\"custom_form_field\">#{label(method, label_text )} #{ form_field }</div>"
+    end
+    
+    def get_dom_id_from(options, method)
+      return options[:id] || object_name + "_" + method.to_s
     end
   end
   
